d3d2621d821e56f72cab1ccba294e4552885f671
BushraBashir901 <bushrabashir901@gmail.com>
2026-01-13 00:02:11 +0500

    Add files via upload

---
+++ Day01.py
@@ -0,0 +1,25 @@
+#..................PRINT FUNCTION....................#
+print("Hello Meissasoft")
+print(1)
+print("1,2,3,4,5")
+#.................Variables......................#
+name="Bushra"
+#..................Data Types..................................#
+a=5 
+b=10
+string="Hello to Interns"
+x=True
+c=9.93
+list=[1,2,3,4,5]
+#.....................Operators.......................#
+a=3
+b=5
+print(a+b)
+
+
+
+a = int(input("First num"))
+b = int(input("Second num"))
+print(a+b)
+print(a-b)
+print(a*b)
